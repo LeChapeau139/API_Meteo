a907d88425d9140f2ff793f32a46ea342384671b
LeChapeau139 <122629554+LeChapeau139@users.noreply.github.com>
2024-04-02 13:08:36 +0200

    Version 1.3
    Remove the video background and replace it with an image background

--- index.js
+++ index.js
@@ -4,10 +4,9 @@ const weatherBox = document.querySelector('.weather-box');
 const weatherDetails = document.querySelector('.weather-details');
 const error404 = document.querySelector('.not-found');
 
-search.addEventListener('click', () => {
-
+// Fonction pour rechercher la météo
+function searchWeather(city) {
     const APIKey = '449617b922c4e10aa802c200f98db360';
-    const city = document.querySelector('.search-box input').value;
 
     if (city === '')
         return;
@@ -15,44 +14,104 @@ search.addEventListener('click', () => {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`)
         .then(response => response.json())
         .then(json => {
-
             if (json.cod === '404') {
-                container.style.height = '400px';
-                weatherBox.style.display = 'none';
-                weatherDetails.style.display = 'none';
-                error404.style.display = 'block';
-                error404.classList.add('fadeIn');
+                // Gérer l'erreur 404
                 return;
             }
 
-            error404.style.display = 'none';
-            error404.classList.remove('fadeIn');
-
             const image = document.querySelector('.weather-box img');
+            const fond = document.querySelector('#fond');
             const temperature = document.querySelector('.weather-box .temperature');
             const description = document.querySelector('.weather-box .description');
             const humidity = document.querySelector('.weather-details .humidity span');
             const wind = document.querySelector('.weather-details .wind span');
+            const clouds = document.querySelector('.weather-details .clou span');
+            const mains = document.querySelector('.weather-details .basse span');
+            const main = document.querySelector('.weather-details .haute span');
+            const color_times = document.querySelector('.weather-box');
+            const color_daily = document.querySelector('.weather-daily');
+            const color_humidity = document.querySelector('.humidity');
+            const color_wind = document.querySelector('.weather-details .wind');
+            const color_clou = document.querySelector('.weather-details .clou');
+            const color_temp_diff = document.querySelector('.weather-details .temp_diff');
+            const color_map = document.querySelector('.weather-map');
+            const color_box = document.querySelector('.search-box');
+            const color_text = document.querySelector('#recherche');
 
             switch (json.weather[0].main) {
                 case 'Clear':
                     image.src = 'images/clear.png';
+                    fond.src = 'images/ensoleil.png';
+                    color_times.style.backgroundColor = 'rgba(7,28,49,255)';
+                    color_daily.style.backgroundColor = 'rgba(7,28,49,255)';
+                    color_humidity.style.backgroundColor = 'rgba(7,28,49,255)';
+                    color_wind.style.backgroundColor = 'rgba(7,28,49,255)';
+                    color_clou.style.backgroundColor = 'rgba(7,28,49,255)';
+                    color_temp_diff.style.backgroundColor = 'rgba(7,28,49,255)';
+                    color_map.style.backgroundColor = 'rgba(7,28,49,255)';
+                    color_box.style.backgroundColor = 'rgba(7,28,49,255)';
+                    color_text.style.backgroundColor = 'rgba(7,28,49,255)';
                     break;
 
                 case 'Rain':
                     image.src = 'images/rain.png';
+                    fond.src = 'images/blue-rain.png';
+                    color_times.style.backgroundColor = 'rgba(7,28,49,255)';
+                    color_daily.style.backgroundColor = 'rgba(7,28,49,255)';
+                    color_humidity.style.backgroundColor = 'rgba(7,28,49,255)';
+                    color_wind.style.backgroundColor = 'rgba(7,28,49,255)';
+                    color_clou.style.backgroundColor = 'rgba(7,28,49,255)';
+                    color_temp_diff.style.backgroundColor = 'rgba(7,28,49,255)';
+                    color_map.style.backgroundColor = 'rgba(7,28,49,255)';
+                    color_box.style.backgroundColor = 'rgba(7,28,49,255)';
+                    color_text.style.backgroundColor = 'rgba(7,28,49,255)';
                     break;
 
                 case 'Snow':
                     image.src = 'images/snow.png';
+                    video.src = 'images/snowy.jpg';
                     break;
 
                 case 'Clouds':
                     image.src = 'images/cloud.png';
+                    fond.src = 'images/cloudy.jpg';
+                    color_times.style.backgroundColor = 'rgba(7,28,49,255)';
+                    color_daily.style.backgroundColor = 'rgba(7,28,49,255)';
+                    color_humidity.style.backgroundColor = 'rgba(7,28,49,255)';
+                    color_wind.style.backgroundColor = 'rgba(7,28,49,255)';
+                    color_clou.style.backgroundColor = 'rgba(7,28,49,255)';
+                    color_temp_diff.style.backgroundColor = 'rgba(7,28,49,255)';
+                    color_map.style.backgroundColor = 'rgba(7,28,49,255)';
+                    color_box.style.backgroundColor = 'rgba(7,28,49,255)';
+                    color_text.style.backgroundColor = 'rgba(7,28,49,255)';
                     break;
 
-                case 'Haze':
+                case 'Mist':
                     image.src = 'images/mist.png';
+                    fond.src = 'images/misty.jpg';
+                    color_times.style.backgroundColor = 'rgba(7,28,49,255)';
+                    color_daily.style.backgroundColor = 'rgba(7,28,49,255)';
+                    color_humidity.style.backgroundColor = 'rgba(7,28,49,255)';
+                    color_wind.style.backgroundColor = 'rgba(7,28,49,255)';
+                    color_clou.style.backgroundColor = 'rgba(7,28,49,255)';
+                    color_temp_diff.style.backgroundColor = 'rgba(7,28,49,255)';
+                    color_map.style.backgroundColor = 'rgba(7,28,49,255)';
+                    color_box.style.backgroundColor = 'rgba(7,28,49,255)';
+                    color_text.style.backgroundColor = 'rgba(7,28,49,255)';
+                    break;
+
+                case 'Thunder':
+                    image.src = 'images/thunderstorm.png';
+                    fond.src = 'images/thunder.jpeg';
+                    color_times.style.backgroundColor = 'rgba(7,28,49,255)';
+                    color_daily.style.backgroundColor = 'rgba(7,28,49,255)';
+                    color_humidity.style.backgroundColor = 'rgba(7,28,49,255)';
+                    color_wind.style.backgroundColor = 'rgba(7,28,49,255)';
+                    color_clou.style.backgroundColor = 'rgba(7,28,49,255)';
+                    color_temp_diff.style.backgroundColor = 'rgba(7,28,49,255)';
+                    color_map.style.backgroundColor = 'rgba(7,28,49,255)';
+                    color_box.style.backgroundColor = 'rgba(7,28,49,255)';
+                    color_text.style.backgroundColor = 'rgba(7,28,49,255)';
                     break;
 
                 default:
@@ -63,15 +122,17 @@ search.addEventListener('click', () => {
             description.innerHTML = `${json.weather[0].description}`;
             humidity.innerHTML = `${json.main.humidity}%`;
             wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
+            clouds.innerHTML = `${parseInt(json.clouds.all)}%`;
+            mains.innerHTML = `${parseInt(json.main.temp_min)}°C`;
+            main.innerHTML = `${parseInt(json.main.temp_max)}°C`;
 
             weatherBox.style.display = '';
             weatherDetails.style.display = '';
+            weatherDaily.style.display = '';
+            weatherMap.style.display = '';
             weatherBox.classList.add('fadeIn');
             weatherDetails.classList.add('fadeIn');
-            container.style.height = '590px';
-
-
+            weatherDaily.classList.add('fadeIn');
+            weatherMap.classList.add('fadeIn');
         });
-
-
-});
+}
